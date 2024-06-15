3a67a43a90ccbece926b4519a97673f1bc744499
Jack Bogart <jackbogart0822@gmail.com>
2024-06-15 17:11:18 -0400

    Add the task object for storing todo information
    A single todo objective will be referred to as a 'Task', this task
    module exports a class that contains information and provides methods to
    properly control.

---
+++ src/js/task.js
@@ -0,0 +1,71 @@
+import { formatInTimeZone, fromZonedTime } from 'date-fns-tz';
+
+const priorities = ['low', 'medium', 'high'];
+
+export default class Task {
+    #title;
+    #desc;
+    #priority;
+    #dueDate;
+    #completed;
+
+    constructor(title, desc, priority, dueDate) {
+        this.title = title;
+        this.desc = desc;
+        this.priority = priority;
+        this.dueDate = dueDate;
+        this.#completed = false;
+    }
+
+    get title() {
+        return this.#title;
+    }
+
+    set title(newTitle) {
+        this.#title = String(newTitle);
+    }
+
+    get desc() {
+        return this.#desc;
+    }
+
+    set desc(newDesc) {
+        this.#desc = String(newDesc);
+    }
+
+    get priority() {
+        return this.#priority;
+    }
+
+    set priority(priorityLevel) {
+        // Ensure the priority level is one of the available options
+        if (priorities.includes(String(priorityLevel).toLowerCase())) {
+            this.#priority = priorityLevel;
+        } else {
+            throw new Error('Invalid priority level');
+        }
+    }
+
+    get completed() {
+        return this.#completed;
+    }
+
+    toggleCompleted() {
+        this.#completed = !this.completed;
+    }
+
+    get dueDate() {
+        return this.#dueDate;
+    }
+
+    set dueDate(dueDate) {
+        // We want to store all dates in UTC so they can be converted to timezones
+        const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+        this.#dueDate = new Date(fromZonedTime(dueDate, timeZone));
+    }
+
+    getLocalDueDate() {
+        const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+        return formatInTimeZone(this.dueDate, timeZone, 'MMM dd, yyyy');
+    }
+}
