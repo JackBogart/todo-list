0495bf087ee8ea24200d7a190344448f470abcf7
Jack Bogart <jackbogart0822@gmail.com>
2024-06-15 22:10:11 -0400

    Add basic site layout
    Previously all the work done was for the JS end and the page had no
    elements nor styling. This patch implements the bare essentials of both.

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,7 @@
 import 'normalize.css';
 import '../css/styles.css';
 import Project from './project.js';
+import { formatInTimeZone } from 'date-fns-tz';
+const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+document.querySelector('.date').textContent = formatInTimeZone(Date.now(), timeZone, 'MMM dd, yyyy');
