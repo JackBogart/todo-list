7fb1a5316c134c0818de3d5ffd0d46cca2eb30b4
Jack Bogart <jackbogart0822@gmail.com>
2024-07-07 19:23:12 -0400

    Update webpack to minify js
    Noticed that JS wasn't being minified due to how I set up the optimizer.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,36 +1,36 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = merge(common, {
-  mode: "production",
-  devtool: "source-map",
+  mode: 'production',
+  devtool: 'source-map',
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css",
-      chunkFilename: "[id][contenthash].css",
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[id][contenthash].css',
     }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
-        test: /\.js$/, // Apply this rule to .js files
-        exclude: /node_modules/, // Exclude node_modules directory
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: [["@babel/preset-env", { targets: "defaults" }]],
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
           },
         },
       },
     ],
   },
   optimization: {
-    minimizer: [new CssMinimizerPlugin()],
+    minimizer: ['...', new CssMinimizerPlugin()],
   },
 });
